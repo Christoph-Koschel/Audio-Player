e19205c907562ce354ef0535c7d7a4d708ce1bef
Sleepy-Reacon <63058519+Sleepy-Reacon@users.noreply.github.com>
2020-12-02 20:52:56 +0100

    Audio Player 2

--- todo.js
+++ todo.js
@@ -4,3 +4,16 @@
 *  - deactivate devTool (openDevTools())
 *  - clear dist folder
 * */
+
+/*TODO
+*  if bug release change version number:
+*  - before: V1.0.0
+*  - after: V1.0.1
+*  if new function but open old userData files then change version number:
+*  - before: V1.0.3
+*  - after: V1.1.0
+*  if new function but open need new userData files then change version number:
+*  - before: 1.2.5
+*  - after: 2.0.0
+*  https://www.heise.de/developer/artikel/Schrittweise-Versionsnummern-richtig-vergeben-1859566.html
+* */
